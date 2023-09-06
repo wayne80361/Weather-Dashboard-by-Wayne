892b7200c9ee500f3c8940631d164d4275ce0604
Wayne <wayne80361@gmail.com>
2023-09-06 01:14:05 -0400

    add date to every forcast  weather boxes.

--- script.js
+++ script.js
@@ -53,12 +53,62 @@ function getApi() {
 
       console.log(" \\\\\\\\\\ this is 2nd day log \\\\\\\\\\ ");
       console.log(data.list[10]);
+      // date
+      var day1 = document.getElementById("day1");
+      var timeInText = data.list[10].dt_txt;
+      var dateOnly1 = timeInText.substr(0, 10);
+      console.log(dateOnly1);
+      var date1 = document.createElement("p");
+      date1.classList.add("date");
+      date1.innerHTML = `<strong>${dateOnly1}</strong>`;
+      day1.appendChild(date1);
+
       console.log(" \\\\\\\\\\ this is 3rd day log \\\\\\\\\\ ");
       console.log(data.list[18]);
+      // date
+      var day2 = document.getElementById("day2");
+      var timeInText = data.list[18].dt_txt;
+      var dateOnly2 = timeInText.substr(0, 10);
+
+      var date2 = document.createElement("p");
+      date2.classList.add("date");
+      date2.innerHTML = `<strong>${dateOnly2}</strong>`;
+      day2.appendChild(date2);
+
       console.log(" \\\\\\\\\\ this is 4th day log \\\\\\\\\\ ");
-      console.log(data.list[34]);
+      console.log(data.list[26]);
+      // date
+      var day3 = document.getElementById("day3");
+      var timeInText = data.list[26].dt_txt;
+      var dateOnly3 = timeInText.substr(0, 10);
+
+      var date3 = document.createElement("p");
+      date3.classList.add("date");
+      date3.innerHTML = `<strong>${dateOnly3}</strong>`;
+      day3.appendChild(date3);
+
       console.log(" \\\\\\\\\\ this is 5th day log \\\\\\\\\\ ");
+      console.log(data.list[34]);
+      // date
+      var day4 = document.getElementById("day4");
+      var timeInText = data.list[34].dt_txt;
+      var dateOnly4 = timeInText.substr(0, 10);
+
+      var date4 = document.createElement("p");
+      date4.classList.add("date");
+      date4.innerHTML = `<strong>${dateOnly4}</strong>`;
+      day4.appendChild(date4);
+
+      console.log(" \\\\\\\\\\ this is 6th day log \\\\\\\\\\ ");
       console.log(data.list[39]);
+      var day5 = document.getElementById("day5");
+      var timeInText = data.list[39].dt_txt;
+      var dateOnly5 = timeInText.substr(0, 10);
+
+      var date5 = document.createElement("p");
+      date5.classList.add("date");
+      date5.innerHTML = `<strong>${dateOnly5}</strong>`;
+      day5.appendChild(date5);
     });
 }
 
